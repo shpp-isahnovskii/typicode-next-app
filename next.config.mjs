c3e446de39871684925d16c2c5d1724344c6cad5
isakhnovskyi <isakhnovskyi.jobs@gmail.com>
2024-07-04 10:08:18 +0300

    add import to git

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // enable static exports for GHub pages
+  basePath: '/typicode-next-app', // my repo name
+  reactStrictMode: true,
+};
 
 export default nextConfig;
