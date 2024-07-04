f477ff2eace48bf1c42f3cb22b283417495bd2ee
isakhnovskyi <isakhnovskyi.jobs@gmail.com>
2024-07-04 15:33:16 +0300

    comment deploy mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enable static exports for GHub pages
-  basePath: '/typicode-next-app', // my repo name
+  //output: 'export', // enable static exports for GHub pages
+  //basePath: '/typicode-next-app', // my repo name
   reactStrictMode: true,
 };
 
